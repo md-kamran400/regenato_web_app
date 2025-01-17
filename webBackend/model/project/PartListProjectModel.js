d94d9d3f7a6b5b4b2c0b4a3715e0287adfe3d7df
Md-abdul <mdabdulq62@gmail.com>
2025-01-17 17:17:59 +0530

    done all task

--- webBackend/model/project/PartListProjectModel.js
+++ webBackend/model/project/PartListProjectModel.js
@@ -1,68 +1,5 @@
-// const mongoose = require("mongoose");
-
-// // Part schema nested inside Project schema
-// const partSchema = new mongoose.Schema({
-//   Uid: String,
-//   partName: String,
-//   codeName: String,
-//   costPerUnit: Number,
-//   timePerUnit: Number,
-//   quantity: Number,
-//   rmVariables: [
-//     {
-//       name: String,
-//       netWeight: Number,
-//       pricePerKg: Number,
-//       totalRate: Number,
-//     },
-//   ],
-//   manufacturingVariables: [
-//     {
-//       name: String,
-//       hours: Number,
-//       times: String,
-//       hourlyRate: Number,
-//       totalRate: Number,
-//     },
-//   ],
-//   shipmentVariables: [
-//     {
-//       name: String,
-//       hourlyRate: Number,
-//       totalRate: Number,
-//     },
-//   ],
-//   overheadsAndProfits: [
-//     {
-//       name: String,
-//       percentage: Number,
-//       totalRate: Number,
-//     },
-//   ],
-// });
-
-// // New partsListSchema
-// const partsListSchema = new mongoose.Schema({
-//   partsListName: String,
-//   partsListItems: [partSchema],
-// });
-
-// // Modified projectSchema
-// const partprojectSchema = new mongoose.Schema({
-//   projectName: String,
-//   costPerUnit: Number,
-//   timePerUnit: Number,
-//   stockPoQty: Number,
-//   projectType: String,
-//   partsLists: [partsListSchema],
-// });
-
-// const PartListProjectModel = mongoose.model("PartProject", partprojectSchema);
-// module.exports = PartListProjectModel;
-
 const mongoose = require("mongoose");
 
-// Part schema nested inside Project schema
 const partSchema = new mongoose.Schema({
   Uid: String,
   partName: String,
@@ -109,19 +46,6 @@ const partsListSchema = new mongoose.Schema({
   partsListItems: [partSchema],
 });
 
-// Modified projectSchema
-// const partprojectSchema = new mongoose.Schema({
-//   projectName: String,
-//   costPerUnit: Number,
-//   timePerUnit: Number,
-//   stockPoQty: Number,
-//   projectType: String,
-//   partsLists: [partsListSchema],
-//   machineHours: {
-//     type: Object,
-//     default: {},
-//   },
-// });
 const partprojectSchema = new mongoose.Schema({
   projectName: String,
   costPerUnit: Number,
