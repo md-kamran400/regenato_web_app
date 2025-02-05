569adfbbe793321f86a2b825a38d5563014412ac
Md-abdul <mdabdulq62@gmail.com>
2025-02-05 14:38:38 +0530

    fix issue

--- material/src/index.js
+++ material/src/index.js
@@ -45,6 +45,7 @@
 // import "./index.css";
 // import App from "./App";
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -53,7 +54,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <React.Fragment>
