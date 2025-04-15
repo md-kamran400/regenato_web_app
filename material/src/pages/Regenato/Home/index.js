4e5577001722806faeb11241363eec7e73aa2450
Md Kamran <ka5452488@gmail.com>
2025-04-15 11:13:34 +0530

    new commt

--- material/src/pages/Regenato/Home/index.js
+++ material/src/pages/Regenato/Home/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Card, CardBody, CardHeader, Col, Container, Row } from "reactstrap";
 import BreadCrumb from "../../../Components/Common/BreadCrumb";
 import Widgets from "./Widgets";
-// import { Advanced } from "./TimelineCharts";
-// import ProjectsOverview from "./ProjectsOverview";
-// import UpcomingSchedules from "./UpcomingSchedules";
-// import ActiveProjects from "./ActiveProjects";
-// import ProjectsStatus from "./ProjectsStatus";
+
 const Home = () => {
   // document.title="Projects | Velzon - React Admin & Dashboard Template";
   return (
@@ -19,39 +15,6 @@ const Home = () => {
               <Widgets />
             </Col>
           </Row>
-          {/* <Row>
-            <Col lg={8}>
-              <Card>
-                <CardHeader>
-                  <h4 className="card-title mb-0">
-                    Advanced Timeline (Multiple Range)
-                  </h4>
-                </CardHeader>
-                <CardBody>
-                  <Advanced dataColors='["--vz-primary", "--vz-success", "--vz-warning"]' />
-                </CardBody>
-              </Card>
-            </Col>
-            <Col lg={4}>
-              <Card>
-                <UpcomingSchedules />
-              </Card>
-            </Col>
-          </Row>
-          <Row className="project-wrapper">
-            <Col xxl={8}>
-              <ProjectsOverview />
-            </Col>
-          </Row>
-
-          <Row className="project-wrapper">
-            <Col lg={8}>
-              <ActiveProjects />
-            </Col>
-            <Col lg={4}>
-              <ProjectsStatus />
-            </Col>
-          </Row> */}
         </Container>
       </div>
     </React.Fragment>
